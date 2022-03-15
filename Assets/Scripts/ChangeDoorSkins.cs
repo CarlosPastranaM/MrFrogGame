ff9c039b815d8542990067a73343affede8ac0a4
Carlos Pastrana <carlos.pastrana.ti@gmail.com>
2022-03-14 19:54:32 -0600

    Juego con Unity Hub

---
+++ Assets/Scripts/ChangeDoorSkins.cs
@@ -0,0 +1,55 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChangeDoorSkins : MonoBehaviour
+{
+    public GameObject skinsPanel;
+    private bool inDoor = false;
+    public GameObject player;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            skinsPanel.gameObject.SetActive(true);
+            inDoor = true;
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        skinsPanel.gameObject.SetActive(false);
+            inDoor = false;
+    }
+
+    public void SetPlayerVirtualGuy()
+    {
+        PlayerPrefs.SetString("PlayerSelected", "VirtualGuy"); 
+        ResetPlayerSkin();
+    }
+
+    public void SetPlayerMaskDude()
+    {
+        PlayerPrefs.SetString("PlayerSelected", "MaskDude"); 
+        ResetPlayerSkin();
+    }
+
+    public void SetPlayerNinjaFrog()
+    {
+        PlayerPrefs.SetString("PlayerSelected", "NinjaFrog"); 
+        ResetPlayerSkin();
+    }
+
+    public void SetPlayerPinkMan()
+    {
+        PlayerPrefs.SetString("PlayerSelected", "PinkMan"); 
+        ResetPlayerSkin();
+    }
+
+    void ResetPlayerSkin()
+    {
+        skinsPanel.gameObject.SetActive(false);
+        player.GetComponent<PlayerSelect>().ChangePlayerInMenu();
+    }
+}
